848808a82e71ccccb692bc9b2d0783813fe872ac
Nick Morris <nicholasmorris2599@gmail.com>
2023-01-13 12:56:15 -0800

    Adjusted employee and engineer class to accurately display the getRole function.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const Employee = require('./lib/employee')
+const Engineer = require('./lib/engineer')
 
 const newEmployee = new Employee('nick', 23, '@gmail');
+const newEngineer = new Engineer('nick morris', 24, '@gmail', 'volexity22')
 
-newEmployee.getName();
+newEmployee.getRole();
+newEngineer.getRole();
