585ec7251d91cb4b2d762e0df083ed8bc5b5fdcd
Nick Morris <nicholasmorris2599@gmail.com>
2023-01-14 11:10:39 -0800

    Added manager class extending from employee.

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const Employee = require('./lib/employee')
 const Engineer = require('./lib/engineer')
+const Manager = require('./lib/manager')
 
 const newEmployee = new Employee('nick', 23, '@gmail');
 const newEngineer = new Engineer('nick morris', 24, '@gmail', 'volexity22')
+const newManager = new Manager('nick morris', 24, '@gmail', 'volexity22')
 
 newEmployee.getRole();
 newEngineer.getRole();
+newManager.getRole();
