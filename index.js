f0bbdc4b625d7443a81050b6ec1b7d9f27bb4aea
Nick Morris <nicholasmorris2599@gmail.com>
2023-01-14 11:13:10 -0800

    Added intern class extending from employee.

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const Employee = require('./lib/employee')
 const Engineer = require('./lib/engineer')
 const Manager = require('./lib/manager')
+const Intern = require('./lib/intern')
 
 const newEmployee = new Employee('nick', 23, '@gmail');
 const newEngineer = new Engineer('nick morris', 24, '@gmail', 'volexity22')
 const newManager = new Manager('nick morris', 24, '@gmail', 'volexity22')
+const newIntern = new Intern('nick morris', 24, '@gmail', 'volexity22')
 
 newEmployee.getRole();
 newEngineer.getRole();
 newManager.getRole();
+newIntern.getRole();
