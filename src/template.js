56ceade8c021e050ba39908f9dbf98cb97a2b612
Nick Morris <nicholasmorris2599@gmail.com>
2023-01-14 23:42:06 -0800

    Added more styles to the css file.

--- src/template.js
+++ src/template.js
@@ -1,35 +1,42 @@
 // Template for created team profile
 const generateHTML = (input) => {
-return `<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Team Profile</title>
-    <link rel="stylesheet" href="../dist/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-  </head>
-  <body>
-    <header>My Team</header>
-    ${input}
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-  </body>
+  return `
+<!doctype html>
+  <html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>Team Profile</title>
+      <link rel="stylesheet" href="../dist/style.css">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    </head>
+    <body>
+      <header>My Team</header>
+      <section class="custom section">
+      ${input}
+      </section>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    </body>
 </html>`
 }
 
 const generateCard = (member) => {
   let roleQ;
+  let roleEmoji;
 
-  if(member.getRole() === 'Engineer') {
+  if (member.getRole() === 'Engineer') {
     roleQ = `GitHub: <a href="https://github.com/${member.getGithub()}">${member.getGithub()}</a>`
+    roleEmoji = `👓`
   }
-  
-  if(member.getRole() === 'Intern') {
+
+  if (member.getRole() === 'Intern') {
     roleQ = `Education at: ${member.getSchool()}`
+    roleEmoji =`📖`
   }
-  
-  if(member.getRole() === 'Manager') {
+
+  if (member.getRole() === 'Manager') {
     roleQ = `Office number: ${member.getOfficeNum()}`
+    roleEmoji = `🍵`
   }
 
   return `<div class="card" style="width: 18rem;">
