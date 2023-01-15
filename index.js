036fba4724a58e2747a3832160c5f41eddf5e8b7
Nick Morris <nicholasmorris2599@gmail.com>
2023-01-14 19:36:53 -0800

    Added first basic questions through prompts using async and await.

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const Employee = require('./lib/employee')
-const Engineer = require('./lib/engineer')
-const Manager = require('./lib/manager')
-const Intern = require('./lib/intern')
+const Engineer = require('./lib/engineer');
+const Manager = require('./lib/manager');
+const Intern = require('./lib/intern');
 
-const newEmployee = new Employee('nick', 23, '@gmail');
-const newEngineer = new Engineer('nick morris', 24, '@gmail', 'volexity22')
-const newManager = new Manager('nick morris', 24, '@gmail', 'volexity22')
-const newIntern = new Intern('nick morris', 24, '@gmail', 'volexity22')
+// const newEmployee = new Employee('nick', 23, '@gmail');
+// const newEngineer = new Engineer('nick morris', 24, '@gmail', 'volexity22')
+// const newManager = new Manager('nick morris', 24, '@gmail', 'volexity22')
+// const newIntern = new Intern('nick morris', 24, '@gmail', 'volexity22')
+
+const questions = async () => {
+    const answers = await inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is your name?', 
+                name: 'name'
+            },
+            {
+                type: 'input',
+                message: 'What is your team Id?',
+                name: 'id'
+            },
+            {
+                type: 'input',
+                message: 'What is your email?',
+                name: 'email'
+            },
+            {
+                type: 'list',
+                message: 'What is your role with the team?',
+                name: 'role',
+                choices: ['Engineer', 'Intern', 'Manager']
+            }
+        ])
+}
